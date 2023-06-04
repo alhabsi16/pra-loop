5937b0d992dc5101cbdde75a502d1a46ae80d7f4
alhabsi16 <mxmx8283@gmail.com>
2023-06-04 22:02:48 +0400

    odd number

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace pra_loop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Start Number: ");
+            int a = int.Parse (Console.ReadLine());
+
+            Console.Write("End Number: ");
+            int b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.WriteLine("=========================");
+            Console.WriteLine();
+
+            for(int i=a;  i <= b ; i+=2)
+            {
+                
+                Console.WriteLine(i);
+                
+            }
+
+        }
+    }
+}
